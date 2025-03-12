e6ceb9cd87ab23b846c3bc2f321b6e23a2c2f1d9
zehan12 <zehan9211@gmail.com>
2025-03-13 03:32:49 +0530

    feat: :art: add tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<main class="main flex justify-center items-center">
-    <section>
-        <div>Hello svelte</div>
-    </section>
+<main class="main flex items-center justify-center">
+	<section>
+		<h1 class="text-3xl text-purple-500 bg-pink-400 p-3 font-bold underline">
+            Hello World
+        </h1>
+	</section>
 </main>
